f34ce4ece62a3d52e131698ea5b44e9190f4b506
njm1250 <njm1250@gmail.com>
2024-09-25 00:01:02 +0900

    마이크로서비스 프로젝트를 위해 멀티모듈세팅 및 각 서비스에 Dockerfile 설정, Root Directory에 docker-compose 설정

--- order-service/src/main/java/com/ordersystem/order_service/OrderServiceApplication.java
+++ order-service/src/main/java/com/ordersystem/order_service/OrderServiceApplication.java
@@ -2,8 +2,9 @@ package com.ordersystem.order_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 public class OrderServiceApplication {
 
 	public static void main(String[] args) {
