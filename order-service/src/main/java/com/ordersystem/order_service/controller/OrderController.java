cc4c6e1e13f62d33c813198f626fcda139b73962
njm1250 <njm1250@gmail.com>
2024-10-01 19:33:50 +0900

    Refactored test order create method

--- order-service/src/main/java/com/ordersystem/order_service/controller/OrderController.java
+++ order-service/src/main/java/com/ordersystem/order_service/controller/OrderController.java
@@ -5,28 +5,36 @@ import com.ordersystem.order_service.entity.OrderStatus;
 import com.ordersystem.order_service.service.OrderService;
 import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.ordersystem.common.events.OrderCreatedEvent;
 
 @RestController
-@RequestMapping("/order")
+@RequestMapping("/orders")
 public class OrderController {
 
     @Autowired
     private OrderService orderService;
 
-    @GetMapping("/test")
-    public ResponseEntity<String> test() {
-        Order order = Order.builder()
-                .productCode("P12345")
-                .quantity(1)
-                .price(1000)
-                .orderStatus(OrderStatus.PENDING)
-                .build();
-        orderService.createOrder(order);
-        return ResponseEntity.ok("test");
+    @PostMapping
+    public ResponseEntity<String> createOrder() {
+        try {
+            Order order = Order.builder()
+                    .productCode("P001")
+                    .quantity(1)
+                    .price(1000)
+                    .orderStatus(OrderStatus.PENDING)
+                    .build();
+
+            orderService.createOrder(order);
+            return ResponseEntity.status(HttpStatus.CREATED).body("Order created successfully");
+
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Failed to create order: " + e.getMessage());
+        }
     }
 }
