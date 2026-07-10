d28e898fb7d180aa10a54d75a10be11bcfa6e38a
njm1250 <njm1250@gmail.com>
2026-07-10 13:29:22 +0900

    feat: configurable kafka topic partition count
    app.kafka.partitions 설정으로 토픽 파티션 수를 조정할 수 있게 한다 (기본 3)

--- order-service/src/main/java/com/ordersystem/order_service/config/KafkaTopicConfig.java
+++ order-service/src/main/java/com/ordersystem/order_service/config/KafkaTopicConfig.java
@@ -4,12 +4,17 @@ import com.ordersystem.common.events.Topics;
 import org.apache.kafka.clients.admin.NewTopic;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.config.TopicBuilder;
 
 @Configuration
 public class KafkaTopicConfig {
 
-    private static final int PARTITIONS = 3;
+    private final int PARTITIONS;
+
+    public KafkaTopicConfig(@Value("${app.kafka.partitions:3}") int partitions) {
+        this.PARTITIONS = partitions;
+    }
 
     @Bean
     public NewTopic orderCreatedTopic() {
